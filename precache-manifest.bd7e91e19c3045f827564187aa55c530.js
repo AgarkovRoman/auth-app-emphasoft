70821b7f8a5bb6967767be6d9aa452a6b20726c9
Roman Agarkov <agarkovromachka@mail.ru>
2020-08-03 13:45:00 +0300

    Updates

---
+++ precache-manifest.bd7e91e19c3045f827564187aa55c530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda8c4bf27c964e0dce23d64d07b9e33",
+    "url": "/auth-app-emphasoft/index.html"
+  },
+  {
+    "revision": "1c39b3889b6a09f2b08c",
+    "url": "/auth-app-emphasoft/static/css/main.ddb038f9.chunk.css"
+  },
+  {
+    "revision": "f0ef70ee1ef8187b3cce",
+    "url": "/auth-app-emphasoft/static/js/2.20bfe25b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/auth-app-emphasoft/static/js/2.20bfe25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c39b3889b6a09f2b08c",
+    "url": "/auth-app-emphasoft/static/js/main.4031db04.chunk.js"
+  },
+  {
+    "revision": "1379594f8b8b46a05bba",
+    "url": "/auth-app-emphasoft/static/js/runtime-main.c3763952.js"
+  }
+]);
